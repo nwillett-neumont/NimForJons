3657c55440679606252de9bee97006f4e82318ed
JustCallMeJon <jveith30@gmail.com>
2026-04-15 11:41:56 -0600

    basic implementation of UML

--- scripts/display.js
+++ scripts/display.js
@@ -1,1 +1,11 @@
 // Display and User Input Module
+// const container = document.getElementById("button-container");
+function updateCollection(){}
+function updateHistory(){}
+function updateCurrentTurn(){}
+function showWinner(){}
+function promptStartNewGame(){}
+function promptContinueToMainMenu(){}
+function showGameUI(){}
+function changePlayButton(){}
+function handlePlayInput(){}
