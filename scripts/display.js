d6c19c61cace88bec99aaf2fa30a5b60c607db72
Michael Vanatta <michaelvanatta12@gmail.com>
2026-04-16 15:28:50 -0600

    Made changes to html and display

--- scripts/display.js
+++ scripts/display.js
@@ -1,44 +1,42 @@
 // Display and User Input Module
-const container = document.getElementById("button-container");
-const collection;
-const startButton;
-const gameHistory;
-const endTurn;
-const firstPlayerRadio;
-
+const collection = document.getElementById("collection");
+const startButton = document.getElementById("startButton");
+const gameHistory = document.getElementById("winLossHistory");
+const endTurnButton = document.getElementById("endTurnButton");
+const firstPlayerRadio = document.getElementsByName("first_turn");
 
 function updateCollection() {
 
-}
+};
 
 function updateHistory() {
 
-}
+};
 
 function updateCurrentTurn() {
 
-}
+};
 
 function showWinner() {
 
-}
+};
 
 function promptStartNewGame() {
 
-}
+};
 
 function promptContinueToMainMenu() {
 
-}
+};
 
 function showGameUI() {
 
-}
+};
 
 function changePlayButton() {
 
-}
+};
 
 function handlePlayInput() {
     
-}
+};
