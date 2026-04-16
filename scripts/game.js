4d598c0b976aff44bc2ed5c9d2fd1940191162fb
= <=>
2026-04-16 16:57:02 -0600

    Slightly modified opponent to fit the needs of game. Most independent functions and variables.

--- scripts/game.js
+++ scripts/game.js
@@ -1,22 +1,31 @@
-// Game Logic Controller Module
-import Opponent from "./opponent";
+import Opponent from "./opponent.js";
 
-var currentPlayer;
-var playerWon;
+// Game Logic Controller Module
+
+var currentPlayer = false;
+var playerWon = false;
 var currentTurn = 0;
-var collection;
-var opponent = new Opponent;
+var collection = [];
+var opponent = Opponent;
 var gameIsBeingPlayed = false;
 
 function startGame() {
+    randomizeCollection();
     gameIsBeingPlayed = true;
+    
     while (gameIsBeingPlayed) {
-        updateCurrentTurn();
+        if (currentPlayer) {
+            // player logic
+        }
+        else {
+            collection = opponent.opponentSelection(collection);
+        }
     }
 };
 
 function alternateTurn() {
-
+    updateCurrentTurn();
+    currentPlayer = !currentPlayer;
 };
 
 function updateCurrentTurn() {
@@ -24,5 +33,14 @@ function updateCurrentTurn() {
 };
 
 function randomizeCollection() {
+    let randomNumber = Math.random() * 6 + 1;
 
+    for (let i = 0; i < randomNumber; i++) {
+        let valuesInCollection = Math.random() * 10 + 1;
+        let subCollection = [];
+        for (let i = 0; i < valuesInCollection; i++) {
+            subCollection.push(document.createElement('button'));
+        }
+        collection.push(subCollection);
+    }
 };
