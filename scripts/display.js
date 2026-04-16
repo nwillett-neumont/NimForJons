4b28983673b9bbb191aa26618769f9de1ede3bdf
= <=>
2026-04-16 17:41:54 -0600

    implemented the first five functions of display.js

--- scripts/display.js
+++ scripts/display.js
@@ -1,28 +1,40 @@
 // Display and User Input Module
-const collection = document.getElementById("collection");
+import {currentPlayer, playerWon, currentTurn, collection} from './game.js'
+
+const displayCollection = document.getElementById("collection");
 const startButton = document.getElementById("startButton");
 const gameHistory = document.getElementById("winLossHistory");
+const displayCurrentTurn = document.getElementById("currentTurn");
+const displayCurrentPlayer = document.getElementById("currentPlayer");
 const endTurnButton = document.getElementById("endTurnButton");
 const firstPlayerRadio = document.getElementsByName("first_turn");
 
 function updateCollection() {
-
+    displayCollection.append(collection);
 };
 
-function updateHistory() {
+function updateHistory() { // not needed
 
 };
 
 function updateCurrentTurn() {
-
+    displayCurrentTurn.innerText = currentTurn;
+    displayCurrentPlayer.innerText = (currentPlayer) ? "Player" : "Opponent";
 };
 
 function showWinner() {
-
+    displayCurrentPlayer.innerText = (playerWon) ? "You Win!" : "You Lose!";
 };
 
 function promptStartNewGame() {
+    displayCollection.hidden = true;
+    displayCurrentTurn.hidden = true;
+    displayCurrentPlayer.hidden = true;
+    endTurnButton.hidden = true;
 
+    gameHistory.hidden = false;
+    firstPlayerRadio.hidden = false;
+    startButton.hidden = false;
 };
 
 function promptContinueToMainMenu() {
@@ -37,6 +49,6 @@ function changePlayButton() {
 
 };
 
-function handlePlayInput() {
+function handlePlayerInput() {
     
 };
